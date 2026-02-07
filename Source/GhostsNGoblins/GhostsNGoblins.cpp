ad9d37918d9ce9f7133bdb4bbbd6b0787a13d5eb
MissHertz <286863@stud.inn.no>
2026-02-07 14:52:24 +0100

    Revert "Git Set-up"
    This reverts commit 0ae567c469791e636e9ff78c1c0c7f239cb66b53.

--- Source/GhostsNGoblins/GhostsNGoblins.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GhostsNGoblins.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GhostsNGoblins, "GhostsNGoblins" );
-
-DEFINE_LOG_CATEGORY(LogGhostsNGoblins)
