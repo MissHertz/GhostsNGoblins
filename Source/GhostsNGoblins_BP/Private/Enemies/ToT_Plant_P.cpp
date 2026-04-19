f30cd5cd7c4ec17a9a3d7d4e762100b98fddd6a2
Aasta-EB <286852@stud.inn.no>
2026-04-19 14:45:20 +0200

    Creating and setting the plant parent
    The plant is now translated into c++, it only needs to die luckily (when attacked)

---
+++ Source/GhostsNGoblins_BP/Private/Enemies/ToT_Plant_P.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemies/ToT_Plant_P.h"
+
+
+// Sets default values
+AToT_Plant_P::AToT_Plant_P()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	OnTakeAnyDamage.AddDynamic(this, &AToT_Plant_P::PlantAttacked);
+}
+
+// Called when the game starts or when spawned
+void AToT_Plant_P::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AToT_Plant_P::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AToT_Plant_P::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AToT_Plant_P::PlantAttacked(AActor* DamagedActor, float Damage, const class UDamageType* DamageType,
+	class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (CurrentHealth > 0)
+	{
+		CurrentHealth -= Damage;
+		if (CurrentHealth <= 0)
+		 {
+			this->Destroy(); 
+		 }
+	}
+}
+
