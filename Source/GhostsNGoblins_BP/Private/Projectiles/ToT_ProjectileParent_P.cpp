885c273c58272a14d2e4b85be5207fbf8eb2632b
Aasta-EB <286852@stud.inn.no>
2026-04-21 14:49:24 +0200

    Creating and translating the code for the projectile parent

---
+++ Source/GhostsNGoblins_BP/Private/Projectiles/ToT_ProjectileParent_P.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectiles/ToT_ProjectileParent_P.h"
+
+#include "Enemies/ToT_EnemyParent_P.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AToT_ProjectileParent_P::AToT_ProjectileParent_P()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	WeaponDamage = 20.f; 
+	ExsistanceTime = 2.f; 
+}
+
+// Called when the game starts or when spawned
+void AToT_ProjectileParent_P::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AToT_ProjectileParent_P::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	ExsistanceTime -= DeltaTime; 
+	if (ExsistanceTime < 0)
+	{
+		this->Destroy();
+	}
+}
+
+void AToT_ProjectileParent_P::HitEnemy(UPrimitiveComponent* HitComponent, AActor* HitActor,
+	UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (HitActor and EnemyBlueprint and HitActor->IsA(EnemyBlueprint))
+	{
+		UGameplayStatics::ApplyDamage(HitActor, WeaponDamage, GetInstigatorController(), this, UDamageType::StaticClass());
+	}
+}
+
---
+++ Source/GhostsNGoblins_BP/Public/Projectiles/ToT_ProjectileParent_P.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "ToT_ProjectileParent_P.generated.h"
+
+UCLASS()
+class GHOSTSNGOBLINS_BP_API AToT_ProjectileParent_P : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AToT_ProjectileParent_P();
+	
+	// Properties of the actor
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* Projectile;
+	
+	UPROPERTY(EditAnywhere)
+	USphereComponent* CollisionSphere;
+	
+	UPROPERTY(EditAnywhere)
+	UProjectileMovementComponent* ProjectileMovement; 
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Reference to the Enemy blueprint (not the enemy directly)
+	UPROPERTY(EditAnywhere, Category = "Enemy")
+	TSubclassOf<ACharacter> EnemyBlueprint;
+	
+	// Variables
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float WeaponDamage;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float ExsistanceTime;
+	
+	// Function for when the projectile hits the enemy
+	void HitEnemy(UPrimitiveComponent* HitComponent, AActor* HitActor,
+	UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+};
