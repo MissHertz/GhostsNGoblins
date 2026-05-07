2dc5dcbae84a40f4047687c19b28f725860cfdd3
Aasta-EB <286852@stud.inn.no>
2026-05-07 08:25:03 +0200

    Cleanup of code and comments

--- Source/GhostsNGoblins_BP/Private/Objects/ToT_WaterCollider_P.cpp
+++ Source/GhostsNGoblins_BP/Private/Objects/ToT_WaterCollider_P.cpp
@@ -12,12 +12,15 @@ AToT_WaterCollider_P::AToT_WaterCollider_P()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
+	// Creating the collision box
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisonBox"));
 	RootComponent = CollisionBox;
 	CollisionBox->SetRelativeScale3D(FVector(1.f, 1.f, 0.3));
 	
+	// Binding collision box on overlap to on player overlap function
 	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AToT_WaterCollider_P::PlayerOverlapping);
 	
+	// Setting default damage value
 	Damage = 2; 
 }
 
@@ -34,9 +37,11 @@ void AToT_WaterCollider_P::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+// On player overlapping function
 void AToT_WaterCollider_P::PlayerOverlapping(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// Checking if overlapping actor is the player, if true applying damage to player
 	if (OtherActor->IsA(PlayerBlueprint))
 	{
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
